073b50dc57277233d0928fd24ca758d9b13f1d1c
DenisAbashkin <35079479+DenisAbashkin@users.noreply.github.com>
2021-12-17 01:29:28 +0300

    write binary tree and Depth_First_Search (Поиск в глубину)

---
+++ src/com/company/Depth_First_Search.java
@@ -0,0 +1,43 @@
+package com.company;
+
+public class Depth_First_Search {//Алгоритм поиска по дереву в глубину
+    public static  void main(String[] params) {
+        Tree root =
+                new Tree(20,
+                        new Tree(7,
+                                new Tree(4, null, new Tree(6)), new Tree(9)),
+                        new Tree(35,
+                                new Tree(31, new Tree(28), null),
+                                new Tree(40, new Tree(38), new Tree(52))));//Это дерево написано вручную, посмотреть на него можно в readme
+
+        System.out.println("Сумма дерева: " + root.sum());
+    }
+
+    static class Tree{
+        int value;
+        Tree left;
+        Tree right;
+
+        public Tree(int value, Tree left, Tree right) {
+            this.value = value;
+            this.left = left;
+            this.right = right;
+        }
+
+        public Tree(int value) {//для конечного(вроде)
+            this.value = value;
+        }
+        public int sum() {//Рекурсивный обход дерева в глубину
+            int summ = value;
+
+            if (left != null) {
+                summ += left.sum();
+            }
+
+            if (right != null) {
+                summ += right.sum();
+            }
+            return summ;
+        }
+    }
+}
