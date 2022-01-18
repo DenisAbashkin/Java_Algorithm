4189b2c8742b609f2010fd7aa6db65ae3006a998
DenisAbashkin <35079479+DenisAbashkin@users.noreply.github.com>
2022-01-19 02:47:19 +0300

    Add stream api with length

---
+++ src/com/company/Search_on_List.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Search_on_List {
+
+    public static void main(String[] args) {
+        List<String> countries = Arrays.asList("Germany", "Panama", "Australia", "One", "Two");
+
+        //Метод через stream
+        Stream stream = countries.stream();
+        stream.filter(x-> x.toString().length() == 3).forEach(System.out::println);
+
+        //Старый метод
+        for(int i = 0; i < countries.size(); i++) {
+            if (countries.get(i).length() < 4) {
+                System.out.println(countries.get(i));
+            }
+        }
+    }
+}
